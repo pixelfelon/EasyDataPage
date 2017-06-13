3c411e9a23cf6bda5108fe8eba1e415ebef73c9f
James Rowley <james@jrowley.me>
2017-06-13 08:45:47 -0700

    Add functionality of clicking feeling button

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database("feelings.db");
+db.serialize(function(){
+	db.run("CREATE TABLE IF NOT EXISTS station (id INTEGER, name VARCHAR(60), token VARCHAR(60));");
+	db.run("CREATE TABLE IF NOT EXISTS feels (station INTEGER, feeling INTEGER );");
+});
 
 var index = require('./routes/index');
 var buttons = require('./routes/buttons');
@@ -25,6 +31,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/buttons', buttons);
+
+app.post('/button', function(req, res){
+	if(!(req.body.token && req.body.station && req.body.feel)){
+		res.send({error: 'malformed request'})
+		return;
+	}
+	
+	var station_stmt = db.prepare('SELECT * FROM station WHERE name=(?);');
+	station_stmt.get(req.body.station, function(err, data){
+		station_stmt.finalize();
+		
+		if(!data){
+			res.send({error: 'invalid station'});
+			return;
+		}
+		if (data.token != req.body.token) {
+			res.send({error: 'invalid token'});
+			return;
+		}
+		
+		var feel = parseInt(req.body.feel);
+		var feel_stmt = db.prepare('INSERT INTO feels (station, feeling) VALUES ((?), (?));');
+		feel_stmt.run(data.id, feel);
+		feel_stmt.finalize();
+		
+		console.log("got feeling #"+feel);
+		res.send({success: true, feel: feel});
+	});
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ public/javascripts/buttons.js
@@ -0,0 +1,86 @@
+var station, token;
+var ready = false;
+var thanksNextTimeout = null;
+
+$(setupPrompt);
+
+function setupPrompt(){
+	station = "";
+	token = "";
+	
+	while (station == "") {
+		station = prompt("Enter button station name.");
+	}
+	while (token == "") {
+		token = prompt("Enter authentication token.");
+	}
+	if (station === null || token === null) {
+		alert("Both the station ID and auth token must be configured to use this page. You can re-run this prompt from the menu in the upper right.");
+	} else if (!ready) {
+		ready = true;
+		prepare();
+	}
+}
+
+function prepare(){
+	$('#content').addClass('active');
+	$('.button').each(function(){
+		var button = $(this);
+		var inter = button.attr('id').match(/^b-([0-9]+)$/);
+		if (inter === null) return;
+		var id = parseInt(inter[1]);
+		button.attr('data-button-id', id)
+		button.click(function(){
+			sendFeel(parseInt($(this).attr('data-button-id')));
+		});
+	});
+}
+
+function sendFeel(id){
+	console.log("Sending a "+id+"!");
+	if (!(station && token)) {
+		alert("Both the station ID and auth token must be configured to use this page.");
+		setupPrompt();
+		return;
+	}
+	
+	$.ajax({
+		contentType: 'application/json',
+		data: JSON.stringify({
+			station: station,
+			token: token,
+			feel: id
+		}),
+		dataType: "json",
+		method: "POST",
+		url: "/button",
+		
+		success: function(data){
+			if (data.error) {
+				switch (data.error) {
+					case "invalid station":
+					case "invalid token":
+						alert("The server did not accept your configuration. Please re-enter.");
+						setupPrompt();
+						break;
+					default:
+						console.error("Server error: "+data.error);
+						break;
+				}
+			}
+		}
+	});
+	showThanks();
+}
+
+function showThanks(){
+	if(thanksNextTimeout !== null) {
+		window.clearTimeout(thanksNextTimeout);
+		thanksNextTimeout = null;
+	}
+	var thanks = $("#thanks-msg");
+	thanks.addClass("active");
+	thanksNextTimeout = window.setTimeout(function(){
+		thanks.removeClass("active");
+	}, 1500);
+}
