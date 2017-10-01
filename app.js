e752929de63205f1d39fac89f61cb935e07ff385
James Rowley <james@jrowley.me>
2017-10-01 10:04:40 -0700

    This repository is a trainwreck

--- app.js
+++ app.js
@@ -31,15 +31,15 @@ app.use('/actions', require('./routes/actions'));
 
 // Boilerplate: error handling
 app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 app.use(function(err, req, res, next) {
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
-	res.status(err.status || 500);
-	res.render('error');
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
